b384d321f12eb986931fa3270c381bdf1c0cd993
MetaMelchoir <ctabere@users.noreply.github.com>
2024-07-10 18:04:35 -0400

    card logic broken

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,12 +1,42 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import KittenCard from "./KittenCard";
-
+import { getRancomKittens, getRandomKittens } from "@/services";
 const GameBoard = () => {
-    const kittensArray= [1, 2, 3, 4, 5 ,6, 7, 8, 9, 10, 11, 12];
+  // const kittensArray= [1, 2, 3, 4, 5 ,6, 7, 8, 9, 10, 11, 12];
+  const [numOfKittens, setNumOfKittens] = useState(6);
+  const [kittens, setKittens] = useState([]);
+  const [cardOne, setCardOne] = useState();
+  const [cardTwo, setCardTwo] = useState();
+  const [solvedKittensArray, setSolvedKiittensArray] = useState([]);
+  // function to fetch kittens
+
+  const fetchKittens = async () => {
+    const response = await getRandomKittens(numOfKittens);
+    if (response.isError) {
+      alert(response.error);
+    } else {
+      console.log("API Response:", responose.images);
+    }
+  };
+
+  useEffect(() => {
+    fetchKittens();
+  }, []);
+
   return (
     <div className="flex flex-wrap flex-row gap-4 justify-center w-full">
-      {kittensArray.map((num) => (
-        <KittenCard number={num} />
+      {kittens.map((kitten) => (
+        <KittenCard
+          key={kitten.id}
+          kitten={kitten}
+          cardOne={cardOne}
+          cardTwo={cardTwo}
+          setCardOne={setCardOne}
+          setCardTwo={setCardTwo}
+          solvedKittensArray={solvedKittensArray}
+          setSolvedKittensArray={setSolvedKittensArray}
+        />
       ))}
     </div>
   );
--- src/components/KittenCard.jsx
+++ src/components/KittenCard.jsx
@@ -1,9 +1,18 @@
 import React from "react";
 
-const KittenCard = ({ number }) => {
+const KittenCard = ({ 
+  kitten,
+  cardOne,
+  cardTwo,
+  setCardOne,
+  setCardTwo,
+  solvedKittensArray,
+  setSolvedKittensArray,
+}) => {
   return (
-    <div className="flex h-80 w-80 items-center justify-center bg-secondary rounded-lg">
+    <div className="flex h-80 w-80 items-center justify-center bg-secondary rounded-lg overflow-clip">
       Click Me {number}
+      <img src={kitten.url} className= 'object-cover w-full h-full' />
     </div>
   );
 };
